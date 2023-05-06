66cd6e411a12cc30e1d479c763262b1380871c6b
Kiven Alaric <elmozaro3@gmail.com>
2023-05-05 22:34:56 -0500

    edits on user and home page

--- src/constant.js
+++ src/constant.js
@@ -2,4 +2,5 @@
 const DB_NAME = process.env.REACT_APP_DB_NAME;
 const PASSWORD = process.env.REACT_APP_PASSWORD;
 const USER = process.env.REACT_APP_DB_USER;
-export { DB_NAME, PASSWORD, USER };
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export { DB_NAME, PASSWORD, USER, API_BASE_URL };
